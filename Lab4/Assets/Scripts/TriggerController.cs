a7a765dd444268615f92c3b53da9643668fed183
Jed93 <jedrekcebrat@gmail.com>
2017-04-04 20:25:27 +0200

    GIM - Lab nr 4

---
+++ Lab4/Assets/Scripts/TriggerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<Collider>().CompareTag("Player"))
+		{
+			AudioSource sound = other.GetComponent<AudioSource>();
+			sound.Play();
+		}
+	}
+}
